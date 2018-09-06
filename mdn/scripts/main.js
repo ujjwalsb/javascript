ddb57ed5667d5c79300882e19b104708fb9338c5
ujjwalcrypto <ujjwalsingh15@gmail.com>
2018-09-06 23:35:52 +0530

    onclick queryselector

--- mdn/scripts/main.js
+++ mdn/scripts/main.js
@@ -1,2 +1,14 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello World';
+
+
+function multiply(num1, num2) {
+    var result = num1 * num2;
+    return result;
+}
+console.log(multiply(4,7));
+
+document.querySelector('html').onclick = function() {
+    alert('Stop poking me !');
+}
+
