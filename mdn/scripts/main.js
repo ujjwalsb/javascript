541fb2f1b2c51a3fbdd0ec1519f06e54ba502d36
ujjwalcrypto <ujjwalsingh15@gmail.com>
2018-09-09 16:08:39 +0530

    implemented simple stack & queue

--- mdn/scripts/main.js
+++ mdn/scripts/main.js
@@ -12,3 +12,36 @@ document.querySelector('html').onclick = function() {
     alert('Stop poking me !');
 }
 
+console.log(Date());
+
+var stack = [];
+stack.push(43);
+stack.push(67);
+stack.push(3);
+document.write('<b>Stack Elements:</b> '+stack.join(', '));
+document.write('<br>');
+document.write('<b>Sorted Stack:</b> '+stack.sort().join(', '));
+stack.pop();
+document.write('<br>')
+document.write('<b>Stack Pop Elements:</b> '+stack.join(', '));
+document.write('<br>')
+document.write('<br>')
+
+
+
+var queue = [];
+queue.push(55);
+queue.push(345);
+queue.push(12);
+document.write('<b>Queue Elements:</b> '+queue.join(', '));
+document.write('<br>');
+document.write('<b>Sorted Queue:</b> '+queue.sort().join(', '));
+queue.shift();
+document.write('<br>')
+document.write('<b>Queue Pop Elements:</b> '+queue.join(', '));
+document.write('<br>')
+document.write('<br>')
+
+
+
+
